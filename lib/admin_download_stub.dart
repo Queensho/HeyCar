e2d41de12569f75789ed43e23e863c42061550c6
Queensho <65372013+Queensho@users.noreply.github.com>
2026-09-24 09:24:47 +0300

    Add generic admin file download fallback

--- lib/admin_download_stub.dart
+++ lib/admin_download_stub.dart
@@ -3,3 +3,7 @@ import 'dart:typed_data';
 Future<void> saveAdminPng(Uint8List bytes,String fileName) async {
   throw UnsupportedError('PNG download is only available on web.');
 }
+
+Future<void> saveAdminFile(Uint8List bytes,String fileName,String mimeType) async {
+  throw UnsupportedError('File download is only available on web.');
+}
